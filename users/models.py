510036f8d99fe27f21b111c638cf91b5d53ab4dc
a12345 <shu.m2010@yandex.ru>
2025-06-30 16:16:57 +0300

    feat: добавлена модель Payment и настроены сериализаторы курсов с уроками

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from materials.models import Course, Lesson
 from django.db import models
 
 
@@ -16,4 +18,21 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = (
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счёт'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='payments', verbose_name='Пользователь')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True, related_name='payments', verbose_name='Курс')
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, null=True, blank=True, related_name='payments', verbose_name='Урок')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f'{self.user.email} — {self.amount} ₽ ({self.payment_method})'
 
