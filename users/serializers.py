f8d7f89ffdccb9710f78134cde9a5887e086ebf7
a12345 <shu.m2010@yandex.ru>
2025-07-04 13:16:22 +0300

    Добавлена фильтрация и сортировка платежей + история платежей в профиле пользователя

--- users/serializers.py
+++ users/serializers.py
@@ -5,15 +5,17 @@ from .models import Payment
 User = get_user_model()
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['email', 'first_name', 'last_name', 'phone', 'city', 'avatar']
-
-
-
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'payments']
+
 
